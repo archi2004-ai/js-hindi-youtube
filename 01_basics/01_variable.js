1c3e6e35cbc5a523f0c0806ccf8af952787095f2
archi2004-ai <archigarg2502@gmail.com>
2024-08-31 08:29:12 +0000

    dataypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 14333
+let accountEmail = "archigarg@gmail.com"
+var accountPssword = "1234"
+ let accountState;
+accountCity = "jaipur"
+//accountId = 34566
+accountEmail = "hitesh.com"
+accountPssword = "5"
+accountCity = "goa"
+accountState;
+console.log(accountId);
+console.table([accountId,accountPssword,accountCity,accountState]);
+
+
+ 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict";//treat all js code std as new version 
+//alert( 3 +3)// we r using node js not a browser
+let age = 18
+console.log("archi");
+console.log(typeof undefined)
+
