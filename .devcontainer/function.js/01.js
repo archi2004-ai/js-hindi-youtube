17c5db5fc8ba6f40602695a0dce766bb081a72b9
archi2004-ai <archigarg2502@gmail.com>
2024-09-05 16:35:27 +0000

    function

---
+++ .devcontainer/function.js/01.js
@@ -0,0 +1,16 @@
+function addTwoNumbers(number1,number2){
+    let res = number1 + number2
+    console.log("archi");
+    return res 
+
+}
+ const res = addTwoNumbers(3,4)
+ console.log( "result is : " ,res)
+    function  loginUserId(username){
+        if (!username){
+            console.log("pls enter username ");
+            return 
+        }
+    return `${username} just logged in `
+ }
+ console.log(loginUserId())
