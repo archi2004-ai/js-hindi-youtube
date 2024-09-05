1c4d1d794b923883effbe424a41f9c17b3c877f3
archi2004-ai <archigarg2502@gmail.com>
2024-09-05 17:05:37 +0000

    caart function

--- .devcontainer/function.js/01.js
+++ .devcontainer/function.js/01.js
@@ -1,11 +1,11 @@
 function addTwoNumbers(number1,number2){
     let res = number1 + number2
-    console.log("archi");
+    //console.log("archi");
     return res 
 
 }
  const res = addTwoNumbers(3,4)
- console.log( "result is : " ,res)
+ //console.log( "result is : " ,res)
     function  loginUserId(username){
         if (!username){
             console.log("pls enter username ");
@@ -13,4 +13,18 @@ function addTwoNumbers(number1,number2){
         }
     return `${username} just logged in `
  }
- console.log(loginUserId())
+ //console.log(loginUserId())
+ function calcartprice(val1 , val2,...num1){
+ return num1
+}
+console.log(calcartprice(200,3000,4000));
+const user = {
+    username : "archi ",
+    price : 78
+}
+function handleobject(anyobj){
+    console.log(`usrname is ${anyobj.username} and price is ${anyobj.price}`);
+}
+    handleobject(user)
+
+
